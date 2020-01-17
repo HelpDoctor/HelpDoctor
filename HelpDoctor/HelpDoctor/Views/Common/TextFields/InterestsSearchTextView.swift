c37ff28c5a9497d7ca84e34192d83a99a48d398f
Mikhail Semerikov <msemerikov@hotmail.com>
2020-01-17 15:20:04 +0500

    1. Изменил работу по поиску, добавлению, редактированию интересов пользователя на экране заполнения профиля 2. Изменил блюр при индикаторе активности на полупрозрачный экран 3. Отключил автокоррекцию при вводе электронной почты на экране регистрации

---
+++ HelpDoctor/HelpDoctor/Views/Common/TextFields/InterestsSearchTextView.swift
@@ -0,0 +1,9 @@
+//
+//  InterestsSearchTextView.swift
+//  HelpDoctor
+//
+//  Created by Mikhail Semerikov on 17.01.2020.
+//  Copyright © 2020 Mikhail Semerikov. All rights reserved.
+//
+
+import Foundation
