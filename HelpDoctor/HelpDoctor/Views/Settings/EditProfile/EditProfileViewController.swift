ede94c5f4666616a79b56ea1b3dbcd95bb73fa54
msemerikov <msemerikov@hotmail.com>
2020-05-22 10:11:05 +0500

    Экран редактирования профиля поправлен в соответствии с дизайном

--- HelpDoctor/HelpDoctor/Views/Settings/EditProfile/EditProfileViewController.swift
+++ HelpDoctor/HelpDoctor/Views/Settings/EditProfile/EditProfileViewController.swift
@@ -15,20 +15,16 @@ class EditProfileViewController: UIViewController {
     
     // MARK: - Constants and variables
     private let headerHeight = 40.f
+    private let titleView = UIView()
+    private let dividerView = UIView()
     private let nameView = UIView()
-    private let nameLabel = UILabel()
     private let contactsView = UIView()
-    private let contactsLabel = UILabel()
     private let educationView = UIView()
-    private let educatiobLabel = UILabel()
     private let jobView = UIView()
-    private let jobLabel = UILabel()
     private let interestsView = UIView()
-    private let interestsLabel = UILabel()
     private let photoView = UIView()
-    private let photoLabel = UILabel()
     private let rowHeight = 50.f
-    private let leading = 20.f
+    private let leading = 10.f
     
     // MARK: - Lifecycle ViewController
     override func viewDidLoad() {
@@ -48,20 +44,69 @@ class EditProfileViewController: UIViewController {
         setupHeaderView(color: .tabBarColor,
                         height: headerHeight,
                         presenter: presenter,
-                        title: "Редактировать профиль",
+                        title: "Редактирование профиля",
                         font: .boldSystemFontOfSize(size: 14))
+        setupTitleView()
+        setupDividerView()
         setupNameView()
         setupView(newView: contactsView, prevView: nameView)
         setupView(newView: educationView, prevView: contactsView)
         setupView(newView: jobView, prevView: educationView)
         setupView(newView: interestsView, prevView: jobView)
         setupView(newView: photoView, prevView: interestsView)
-        setupLabel(label: nameLabel, onView: nameView, text: "ФИО и пол")
-        setupLabel(label: educatiobLabel, onView: educationView, text: "Образование")
-        setupLabel(label: contactsLabel, onView: contactsView, text: "Дата рождения, номер телефона, место жительства")
-        setupLabel(label: jobLabel, onView: jobView, text: "Специализация, место работы")
-        setupLabel(label: interestsLabel, onView: interestsView, text: "Научные интересы")
-        setupLabel(label: photoLabel, onView: photoView, text: "Фото профиля")
+        setupIcon(onView: nameView, iconName: "User_Icon")
+        setupIcon(onView: contactsView, iconName: "Personal_Info_Icon")
+        setupIcon(onView: educationView, iconName: "Education_Icon")
+        setupIcon(onView: jobView, iconName: "Job_Icon")
+        setupIcon(onView: interestsView, iconName: "Interest_Icon")
+        setupIcon(onView: photoView, iconName: "Camera_Icon")
+        setupLabel(onView: nameView, text: "ФИО и пол")
+        setupLabel(onView: educationView, text: "Образование")
+        setupLabel(onView: contactsView, text: "Дата рождения, номер телефона, место жительства")
+        setupLabel(onView: jobView, text: "Специализация, место работы")
+        setupLabel(onView: interestsView, text: "Научные интересы")
+        setupLabel(onView: photoView, text: "Фото профиля")
+    }
+    
+    private func setupTitleView() {
+        let text = """
+        Выберите параметр для внесения изменений в ваш профиль
+        """
+        let label = UILabel()
+        let leading = 10.f
+        
+        titleView.backgroundColor = .white
+        label.textAlignment = .left
+        label.font = .systemFontOfSize(size: 12)
+        label.textColor = .black
+        label.text = text
+        label.numberOfLines = 0
+        view.addSubview(titleView)
+        titleView.addSubview(label)
+        
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        titleView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        titleView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        titleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                            constant: headerHeight).isActive = true
+        titleView.heightAnchor.constraint(equalToConstant: headerHeight).isActive = true
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leading).isActive = true
+        label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -leading).isActive = true
+        label.topAnchor.constraint(equalTo: titleView.topAnchor).isActive = true
+        label.heightAnchor.constraint(equalToConstant: headerHeight).isActive = true
+    }
+    
+    private func setupDividerView() {
+        dividerView.backgroundColor = .searchBarTintColor
+        view.addSubview(dividerView)
+
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        dividerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        dividerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        dividerView.topAnchor.constraint(equalTo: titleView.bottomAnchor).isActive = true
+        dividerView.heightAnchor.constraint(equalToConstant: 10).isActive = true
     }
     
     private func setupNameView() {
@@ -73,8 +118,7 @@ class EditProfileViewController: UIViewController {
         view.addSubview(nameView)
         nameView.translatesAutoresizingMaskIntoConstraints = false
         nameView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        nameView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
-                                      constant: headerHeight).isActive = true
+        nameView.topAnchor.constraint(equalTo: dividerView.bottomAnchor).isActive = true
         nameView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         nameView.heightAnchor.constraint(equalToConstant: rowHeight).isActive = true
     }
@@ -93,7 +137,21 @@ class EditProfileViewController: UIViewController {
         newView.heightAnchor.constraint(equalToConstant: rowHeight).isActive = true
     }
     
-    private func setupLabel(label: UILabel, onView: UIView, text: String) {
+    private func setupIcon(onView: UIView, iconName: String) {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: iconName)
+        onView.addSubview(imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.leadingAnchor.constraint(equalTo: onView.leadingAnchor,
+                                       constant: leading).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: onView.centerYAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+    
+    private func setupLabel(onView: UIView, text: String) {
+        let label = UILabel()
         label.textAlignment = .left
         label.font = .boldSystemFontOfSize(size: 14)
         label.textColor = .white
@@ -103,8 +161,8 @@ class EditProfileViewController: UIViewController {
         
         label.translatesAutoresizingMaskIntoConstraints = false
         label.leadingAnchor.constraint(equalTo: onView.leadingAnchor,
-                                       constant: leading).isActive = true
-        label.trailingAnchor.constraint(equalTo: educationView.trailingAnchor,
+                                       constant: leading + 75).isActive = true
+        label.trailingAnchor.constraint(equalTo: onView.trailingAnchor,
                                         constant: -leading).isActive = true
         label.centerYAnchor.constraint(equalTo: onView.centerYAnchor).isActive = true
         label.heightAnchor.constraint(equalTo: onView.heightAnchor).isActive = true
